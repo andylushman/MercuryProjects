e0b82b0ddc1c726d51c5535143c2d158b56f70a0
Andy Lushman <andylushman@gmail.com>
2018-06-11 14:20:50 -0600

    Added my credientals

--- ChainableWebReader/NodeChainableWebReader/chainableWebReader.js
+++ ChainableWebReader/NodeChainableWebReader/chainableWebReader.js
@@ -1,10 +1,27 @@
+
 var request = require('request-promise');
 
-request({
-  "method":"GET", 
-  "uri": "https://api.github.com/",
-  "json": true,
-  "headers": {
-    "User-Agent": "My little demo app"
+var github = {
+  token: "23f7075b48043e8cc43084098100650e82c8f1d9",
+  
+  getUser: function() {
+    return request({
+      "method":"GET", 
+      "uri": "https://api.github.com/andylushman",
+      "json": true,
+      "headers": {
+        "Authorization": "Bearer " + github.token,
+        "User-Agent": "My little demo app"
+      }
+    });
   }
-}).then(console.log, console.log);
+}
+
+function main(params) {
+  github.token = params.token;
+  return github.getUser();
+}
+
+main({"token": process.argv[2]}).then(function(result) {
+  console.log(result);
+});
