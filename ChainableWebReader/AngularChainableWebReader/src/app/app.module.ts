3d63508fb8c5ff1948b9d2197fe8678517ade747
Andy Lushman <andylushman@gmail.com>
2018-06-11 11:16:45 -0600

    response from Giphy

--- ChainableWebReader/AngularChainableWebReader/src/app/app.module.ts
+++ ChainableWebReader/AngularChainableWebReader/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ResultsComponent } from './results/results.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ResultsComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule
